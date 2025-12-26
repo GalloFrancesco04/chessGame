aaabeb9a21063d4837557ff62d8f75a69027b31a
GalloFrancesco04 <gallofrancesco723@gmail.com>
2025-12-26 21:43:48 +0100

    chore: corrected a type

--- include/Board.h
+++ include/Board.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "enums/Piece.h"
 #include "enums/Color.h"
+#include "Square.h"
 
 class Board
 {
 private:
-    chess::Piece board[8][8];
+    chess::Square board[8][8];
 
 public:
     Board();
-    chess::Piece getPiece(int row, int column);
-    void setPiece(int row, int column, chess::Piece piece);
+    chess::Square getPiece(int row, int column) const;
+    void setSquare(int row, int column, const chess::Square &square);
 };
